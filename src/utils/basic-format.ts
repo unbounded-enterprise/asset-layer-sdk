20368559b3611ea315370d2ef0434da1ddab2db7
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-08-24 12:35:01 -0700

    re-add original qs parsing as default

--- src/utils/basic-format.ts
+++ src/utils/basic-format.ts
@@ -30,6 +30,21 @@ export function basicEncode(key: string, value: any) {
 
 export function propsToQueryString(props?: BasicAnyObject) {
     if (!props) return '';
+    const params = new URLSearchParams();
+
+    for (const key in props) {
+        if (props[key] === undefined) continue;
+        else if (Array.isArray(props[key])) props[key].forEach((value: string) => { if (value) params.append(`${key}[]`, value); });
+        else params.append(key, props[key]);
+    }
+    
+    console.log('qs:', params.toString());
+
+    return '?' + params.toString();
+}
+
+export function propsToQueryString2(props?: BasicAnyObject) {
+    if (!props) return '';
     
     const entries = Object.entries(props);
     const queryProps = [];

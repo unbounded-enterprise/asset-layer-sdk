511283650e06296aa3bdac14098528e7f066d17b
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2023-09-01 19:25:31 -0700

    spacing

--- src/utils/basic-error.ts
+++ src/utils/basic-error.ts
@@ -1,12 +1,12 @@
 import { BasicError } from "../types/basic-types";
 
 export function parseBasicError(error:any, fallbackCode:number = 500): BasicError {
-    if (!error) return new BasicError('Unknown Error', fallbackCode);
+  if (!error) return new BasicError('Unknown Error', fallbackCode);
+
+  const message = error.response?.data?.error || error.response?.data?.message || error.response?.data || error.data?.error 
+    || error.data?.message || error.data?.errorMessage || error.data || error.message || error.error || 'Unknown Error Message';
+  const status = error.response?.data?.statusCode || error.response?.status
+    || error.data?.status || error.data?.statusCode || error.status || fallbackCode;
   
-    const message = error.response?.data?.error || error.response?.data?.message || error.response?.data || error.data?.error 
-      || error.data?.message || error.data?.errorMessage || error.data || error.message || error.error || 'Unknown Error Message';
-    const status = error.response?.data?.statusCode || error.response?.status
-      || error.data?.status || error.data?.statusCode || error.status || fallbackCode;
-    
-    return new BasicError(message, status);
-  }
+  return new BasicError(message, status);
+}
